3f1efb58f3bddc4559ee88fe0bd7f70e9c5fef52
simon.a.wilson <simon.a.wilson@accenture.com>
2023-08-02 11:56:09 +0100

    log42 implementation

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,21 +1,30 @@
 package uk.gov.dwp.uc.pairtest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class TicketServiceImpl implements TicketService {
+    private static Logger logger = LogManager.getLogger(TicketServiceImpl.class.getName());
+//        private static Logger logger = LogManager.getLogger("Application");
+//    private static Logger logger = LogManager.getRootLogger();
+
     /**
      * Should only have private methods other than the one below.
      */
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
         //worth splitting into a separate method
+        //need to do a try catch in case of string input
         if (accountId > 0) {
-
+            logger.debug("Account ID greater than 0");
+            logger.info("Account ID greater than 0");
+            logger.trace("Account ID greater than 0");
         } else {
+            logger.error("Account ID less than 0");
             throw new InvalidPurchaseException("Account ID Invalid: " + accountId);
         }
-
         //need to validate ticket request is valid - max of 20 tickets, 1 child per adult. Clarify infant not buying, still need a ticket.
         //to account for infants having a ticket but not taking up a seat?
         //send request to payment service
