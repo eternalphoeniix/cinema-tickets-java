f80fcfcb0e4a37db5e92d39c75f25f469d6064a2
simon.a.wilson <simon.a.wilson@accenture.com>
2023-08-03 11:28:20 +0100

    Account ID Test parameterised.

--- cinema-tickets-java/src/test/java/TicketServiceImplTest/AccountIdTest.java
+++ cinema-tickets-java/src/test/java/TicketServiceImplTest/AccountIdTest.java
@@ -1,8 +1,9 @@
 package TicketServiceImplTest;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
@@ -10,19 +11,25 @@ import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AccountIdTest {
-    TicketServiceImpl ticketService = new TicketServiceImpl();
+    TicketServiceImpl ticketService;
 
-    @Test
-    @DisplayName("Account ID less than minimum.")
-    void givenAccountIdLessThanOne_whenPurchaseTickets_thenInvalidPurchaseException() {
-        assertThrows(InvalidPurchaseException.class, () ->
-                ticketService.purchaseTickets(0L, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1)));
+    @BeforeEach
+    public void beforeEach() {
+        ticketService = new TicketServiceImpl();
     }
 
-    @Test
-    @DisplayName("Account ID Greater than minimum")
-    void givenAccountIdGreaterThanZero_whenPurchaseTickets_thenAssertTrue() {
-        Assertions.fail();
-        //        ticketService.purchaseTickets(1L, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1));
+    @ParameterizedTest(name = "{index} Invalid Account Number Requested: {0}")
+    @ValueSource(longs = {-1L, 0L})
+    @DisplayName("Invalid Account Numbers")
+    public void givenAccountIdLessThanOne_whenPurchaseTickets_thenInvalidPurchaseException(long accountID) {
+        assertThrows(InvalidPurchaseException.class, () ->
+                ticketService.purchaseTickets(accountID, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1)));
+    }
+
+    @ParameterizedTest(name = "{index} Valid Account Number Requested: {0}")
+    @ValueSource(longs = {1L, 100L, Long.MAX_VALUE})
+    @DisplayName("Valid Account Numbers")
+    public void givenAccountIdGreaterThanZero_whenPurchaseTickets_thenSuccess(long accountID) {
+        ticketService.purchaseTickets(accountID, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1));
     }
 }
